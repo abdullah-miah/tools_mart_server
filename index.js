5ee9dd3e0be06e0cd81183a59127e2ebe0fd2d5f
abdullah-miah <mdabdullaha708@gmail.com>
2022-05-24 13:08:07 +0600

    purchase api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var ObjectId = require('mongodb').ObjectId;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT||5000;
@@ -25,6 +26,12 @@ async function run(){
        const products = await cursor.toArray();
        res.send(products);
    })
+   app.get('/purchase/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const product = await productCollection.findOne(query);
+    res.send(product);
+  })
 
     }
 finally{
