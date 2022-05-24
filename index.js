2a24c2024f5ab676ba9e55ff82f42fae84462e73
abdullah-miah <mdabdullaha708@gmail.com>
2022-05-24 21:09:33 +0600

    delete api

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run(){
     
   })
 
+  // delete api
+  app.delete('/dashboard/:id', async (req, res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await orderCollection.deleteOne(query);
+    res.send(result);
+
+})
+
     }
 finally{
 
