d229b437838de3208e69aa65fa7df7b1e4f097dd
unknown <mdabdullaha708@gmail.com>
2023-04-30 21:44:56 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const port = process.env.PORT||5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ulqow.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ulqow.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = "mongodb+srv://tools_mart_admin:2ElOVnIeqeUHr0MF@cluster0.ulqow.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 function verifyJWT(req, res, next) {
